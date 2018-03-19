583f2fabddedf530110d76e5098ba9331d8f6b30
Austin Heiman <atheimanksu@gmail.com>
2018-03-19 17:03:18 -0500

    simplified spec

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,8 +1,1 @@
-$msi_url = "https://www.7-zip.org/a/7z1801-x64.msi"
-$msi_file = "C:\7z1801-x64.msi"
-
-Write-Host "Downloading MSI package from $msi_url to $msi_file"
-(New-Object System.Net.WebClient).DownloadFile($msi_url, $msi_file)
-
-Write-Host "Installing MSI package from $msi_file"
-Start-Process msiexec.exe -Wait -ArgumentList "/I $msi_file /quiet"
+Start-Process msiexec.exe -Wait -ArgumentList '/i C:\synced_folder\7z1801-x64.msi /quiet'
--- test/integration/default/msi_spec.rb
+++ test/integration/default/msi_spec.rb
@@ -1,50 +1,38 @@
 # https://www.inspec.io/docs/reference/resources/
 
+# package was installed successfully
 describe package('7-Zip 18.01 (x64 edition)') do
   it { should be_installed }
   its('version') { should include('18.01') }
 end
 
-describe powershell("Get-WmiObject -Class Win32_Product") do
-  its('stdout') { should include('7-Zip 18.01') }
-  its('exit_status') { should eq 0 }
-end
-
-describe file('C:/Program Files/7-Zip/7z.exe') do
+# 7z.exe file is available
+describe file('C:\Program Files\7-Zip\7z.exe') do
   it { should exist }
 end
 
-describe command("& \"C:/Program Files/7-Zip/7z.exe\"") do
+# 7z.exe shows the help when called with no args
+describe command('& "C:\Program Files\7-Zip\7z.exe"') do
   its('stdout') { should include('7-Zip 18.01 (x64)') }
   its('stdout') { should include('Usage: 7z') }
   its('exit_status') { should eq 0 }
 end
 
-script = <<-EOH
-  # download zip file
-  (New-Object System.Net.WebClient).DownloadFile(
-    "https://codeload.github.com/atheiman/msi-test/zip/master",
-    "C:/master.zip"
-  )
-  # extract contents of zip file
-  & "C:/Program Files/7-Zip/7z.exe" e C:/master.zip
-EOH
-describe powershell(script) do
-  # its('stdout') { should include('README.md') }
+# 7z.exe successfully opens a zip archive
+describe command(
+  '& "C:\Program Files\7-Zip\7z.exe" e C:\synced_folder\archive.zip -aoa -oC:\\'
+) do
+  its('stdout') { should include('Extracting archive: C:\synced_folder\archive.zip') }
+  its('stdout') { should include('Everything is Ok') }
+  its('stdout') { should include('Files: 2') }
   its('stderr') { should eq('') }
   its('exit_status') { should eq 0 }
 end
 
-describe file('C:/master.zip') do
-  it { should exist }
-end
-
-describe file('C:/msi-test-master/README.md') do
-  it { should exist }
-end
-
-describe command("& \"C:/Program Files/7-Zip/7z.exe\" l C:/master.zip") do
-  its('stdout') { should include(/README\.md/) }
-  its('stderr') { should eq('') }
-  its('exit_status') { should eq 0 }
+# the files from the zip were extracted
+['a', 'b'].each do |f|
+  describe file("C:\\file-#{f}.txt") do
+    it { should exist }
+    its('content') { should cmp("Contents of file-#{f}.txt\r\n") }
+  end
 end
